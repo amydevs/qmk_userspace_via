1894cf306f653b24ad129df358baf00a40f3dd42
Amy Yan <ayanamydev@gmail.com>
2024-09-13 00:30:36 +1000

    wip: lelelab

---
+++ keyboards/lelelab/lele76/keymaps/via/lele76.h
@@ -0,0 +1,10 @@
+typedef enum {
+    id_backlight = 1,
+} via_channel_id;
+
+typedef enum {
+    id_backlight_brightness   = 1,
+    id_backlight_effect       = 2,
+    id_backlight_effect_speed = 3,
+    id_backlight_color        = 4
+} via_backlight_value;
---
+++ keyboards/lelelab/lele76/keymaps/via/rules.mk
@@ -0,0 +1,3 @@
+VIA_ENABLE = yes
+
+SRC += lele76.c screen_app.c tiny_mcu.c
